44674a11aef67ae17f9289d74b005fe0c140a658
m1m0ch1 <103493888+m1m0ch1@users.noreply.github.com>
2022-10-31 09:10:50 +0100

    Add files via upload

---
+++ potlas.js
@@ -0,0 +1,28 @@
+function changeInnerHtml(){
+    document.getElementById('paragraf').innerHTML = "Zöld";
+    let html = "<table  border = '1'>";
+    for (let i = 1; i <=10; i++){
+        html += "<tr>"
+        for (let j = 1; j <= 10; j++) {
+            if (i + j - 1 === 10){
+                html += "<td class = 'piros'>" + "1" +"</td>";
+            }
+            
+            else if (i  === j){
+                html += "<td id = "+i + j+" onmouseover = 'over("+i+"," +j+")'>" + "1" +"</td>";
+            }
+
+            else{ 
+                html += "<td>" + "0" + "</td>";
+            }
+        }
+        html += "</tr>";
+    }
+    html += "</table>";
+    document.getElementById('paragraf').innerHTML = html;
+}
+function over(i, j){
+    id = i.toString() + j;
+    console.log(id);
+    document.getElementById(id).style.backgroundColor = "red";
+}
