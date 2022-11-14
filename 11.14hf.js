f132f8cdd44feaa6783df40d73ae0dd41787683b
m1m0ch1 <103493888+m1m0ch1@users.noreply.github.com>
2022-11-14 14:28:05 +0100

    Add files via upload

---
+++ 11.14hf.js
@@ -0,0 +1,14 @@
+var hfmegoldas = (
+    (1, 'hf óra', '3'),
+    (2, 'hf perc', '47'),
+    (3, 'hf nap', 'vasárnap') )
+
+    tomb = [
+        [ 'Marci', 'Balázs', 'Ádám' ],
+        [ 'Róbert', 'Ákos', 'Patrícia' ],
+        [ 'Kristóf', 'Bence', 'Patrik' ],
+        ];
+
+
+
+
